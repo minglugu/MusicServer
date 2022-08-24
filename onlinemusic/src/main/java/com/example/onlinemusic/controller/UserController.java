cf0c330786aa5365c0778708943daa6b132a8b57
Minglu Gu <gmlsyjsh@gmail.com>
2022-08-23 21:22:39 -0300

    set up login

--- onlinemusic/src/main/java/com/example/onlinemusic/controller/UserController.java
+++ onlinemusic/src/main/java/com/example/onlinemusic/controller/UserController.java
@@ -1,5 +1,33 @@
 package com.example.onlinemusic.controller;
 
+import com.example.onlinemusic.mapper.UserMapper;
+import com.example.onlinemusic.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 // 用户相关的登录
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @RequestMapping("/login")
+    public void login(@RequestParam String username, @RequestParam String password) {
+        User userLogin = new User();
+        userLogin.setUsername(username);
+        userLogin.setPassword(password);
+
+        User user = userMapper.login(userLogin);
+
+        if(user != null) {
+            // 可以优化为，登录成功后，把用户存到session里面
+            System.out.println("Login succeeded");
+        } else {
+            System.out.println("Login failed");
+        }
+    }
 }
---
+++ onlinemusic/src/main/java/com/example/onlinemusic/tools/ResponseBodyMessage.java
@@ -0,0 +1,6 @@
+package com.example.onlinemusic.tools;
+
+// generic class
+public class ResponseBodyMessage <T> {
+    private int status; // status code, such as 1, 2, 3
+}
