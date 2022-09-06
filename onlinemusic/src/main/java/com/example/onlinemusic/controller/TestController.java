0a3bbdffaf6b63342c8cefe00d4617674f2eca5c
Minglu Gu <gmlsyjsh@gmail.com>
2022-09-05 21:57:53 -0300

    modify the login page

---
+++ onlinemusic/src/main/java/com/example/onlinemusic/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.example.onlinemusic.controller;
+
+import com.example.onlinemusic.model.User;
+import com.example.onlinemusic.tools.ResponseBodyMessage;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class TestController {
+
+    @RequestMapping("/login")
+    public ResponseBodyMessage<User> login(@RequestParam String username,
+                                           @RequestParam String password) {
+        System.out.println("username" + username);
+        System.out.println("password" + password);
+
+        User user = new User();
+
+        if(username.equals("bit") && password.equals("123456")) {
+            user.setUsername("bit");
+            user.setPassword("123456");
+
+            return new ResponseBodyMessage<>(0, "Successful login.", user);
+        }else{
+            return new ResponseBodyMessage<>(0, "Failed to login.", null);
+        }
+    }
+}
